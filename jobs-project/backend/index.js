85e174d02cf11ef5537a4e060208ddd7c682cdce
desivar <jillian_desire@yahoo.es>
2025-06-18 21:02:44 -0600

    index

--- jobs-project/backend/index.js
+++ jobs-project/backend/index.js
@@ -1,9 +1,10 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.end('Hello from Node.js!');
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
